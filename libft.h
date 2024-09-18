25317f914a3afa6186dbcc371fa4cf5a58682dc6
yasmin <yasmin27101991@gmail.com>
2024-09-18 19:37:14 +0200

    create strcpy

--- libft.h
+++ libft.h
@@ -15,4 +15,5 @@ void    *ft_memset(void *b, int c, size_t n);
 void    ft_bzero(void *s, size_t n);
 void    *ft_memcpy(void *dest, const void *src, size_t n);
 void    *ft_memmove(void *dest, const void *src, size_t n);
+size_t  ft_strcpy(char *dest, const char *str, size_t n)
 #endif
