da82d5e875bdea697e41a3206f8a3c13de2fa79b
yasmin <yasmin27101991@gmail.com>
2024-09-28 17:20:33 +0200

    testing with francinette

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,16 +6,19 @@
 /*   By: yasjimen <yasjimen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 19:32:19 by yasjimen          #+#    #+#             */
-/*   Updated: 2024/09/26 11:43:17 by yasjimen         ###   ########.fr       */
+/*   Updated: 2024/09/28 17:19:09 by yasjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
+#include "libft.h"
 
 size_t	ft_strlcpy(char *dest, const char *src, size_t n)
 {
 	size_t	i;
 
+	if (n == 0)
+		return (ft_strlen(src));
 	i = 0;
 	if (n > 0)
 	{
